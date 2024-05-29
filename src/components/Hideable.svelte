7121c92814f94add24e9ea40bd5aa778c172ffcc
Riccardo <riccardo.corradin@live.com>
2024-05-29 22:04:17 +0200

    feat: option to pass in class

--- src/components/Hideable.svelte
+++ src/components/Hideable.svelte
@@ -4,7 +4,15 @@
 	const toggleHide = () => (hide = !hide);
 </script>
 
-<div class="group relative" class:web-only={hide} class:text-gray-300={hide} class:line-through={hide} class:bg-black={hide} class:opacity-50={hide} role="button">
+<div
+	class={`${$$restProps.class} group relative`}
+	class:web-only={hide}
+	class:text-gray-300={hide}
+	class:line-through={hide}
+	class:bg-black={hide}
+	class:opacity-50={hide}
+	role="button"
+>
 	<span
 		on:click|stopPropagation={toggleHide}
 		class="select-none cursor-pointer"
